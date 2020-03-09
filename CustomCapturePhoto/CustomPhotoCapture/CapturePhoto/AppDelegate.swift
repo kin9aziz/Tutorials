76b668fd9c1edb968889891d85e34048774ae1db
Quang Tran <quangtqag@gmail.com>
2020-03-09 10:44:31 +0700

    - update

---
+++ CustomCapturePhoto/CustomPhotoCapture/CapturePhoto/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CapturePhoto
+//
+//  Created by Quang Tran on 2/20/19.
+//  Copyright © 2019 Quang Tran. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+  
+}
+
---
+++ CustomCapturePhoto/CustomPhotoCapture/CapturePhoto/PreviewView.swift
@@ -0,0 +1,32 @@
+/*
+ Abstract:
+ Implements the camera preview view that shows capture output.
+ */
+
+import UIKit
+import AVFoundation
+
+class PreviewView: UIView {
+  
+  var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+    guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+      fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
+    }
+    
+    return layer
+  }
+  
+  var session: AVCaptureSession? {
+    get {
+      return videoPreviewLayer.session
+    }
+    set {
+      videoPreviewLayer.session = newValue
+    }
+  }
+  
+  override class var layerClass: AnyClass {
+    return AVCaptureVideoPreviewLayer.self
+  }
+  
+}
